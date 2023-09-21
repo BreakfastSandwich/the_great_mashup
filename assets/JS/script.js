f63b06097afc2d0b7df88a093cc3b224337ae17f
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-09-21 11:04:57 -0400

    commiting before checking old branch

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,58 @@
 // Variable Declaration area*********
 
+// these Var's are connected to the user input form on the search modal
+var flightNumberSearchEl = document.getElementById('flight-search-input')
+var departureCitySearchEl = document.getElementById('departure-search-input')
+var arrivalCitySearchEl = document.getElementById('arrival-search-input')
+
+// previous searches table found in search modal 
+var searchTableBodyEl = document.getElementById('table-body')
+var clearSearchHistoryBtnEl = document.getElementById('clear-search-history')
+var cancelModalBtnEl = document.getElementById('modal-cancel')
+var modalSearchBtnEl = document.getElementById('modal-searchTableBodyEl')
 
 
+// these Var's are the display below the header and above weather 
+var flightNumberPrintEl = document.getElementById('flight-number-print')
+var departureCityPrintEl =document.getElementById('departure-city-print')
+var arrivalCityPrintEl = document.getElementById('arrival-city-print')
+var currentFlightStatusPrintEl = document.getElementById('Current-flight-status')
+
+// departure city weather area
+var departureCityWeatherHeaderEl = document.getElementById('departure-weather-header')
+var departWeatherDay1el = document.getElementById('departure-day-1')
+var departWeatherDay2el = document.getElementById('departure-day-2')
+var departWeatherDay3el = document.getElementById('departure-day-3')
+var departWeatherDay4el = document.getElementById('departure-day-4')
+var departWeatherDay5el = document.getElementById('departure-day-5')
+
+// arrival city weather area 
+var arrivalCityWeatherHeaderEl = document.getElementById('arrival-weather-header')
+var arrivalWeatherDay1el = document.getElementById('arrival-day-1')
+var arrivalWeatherDay2el = document.getElementById('arrival-day-2')
+var arrivalWeatherDay3el = document.getElementById('arrival-day-3')
+var arrivalWeatherDay4el = document.getElementById('arrival-day-4')
+var arrivalWeatherDay5el = document.getElementById('arrival-day-5')
+
+
+// Declaring the "search object" so it can be prepared for later use in the API's
+    //and for storage/ recall of previous searches
+var previousFlight = {
+
+
+}
+
+departureCityWeatherHeaderEl.textContent = "seatle weather 9/21/23"
+
+
+
+
+
+
+// modalSearchBtnEl.addEventListener('click', () => {
+// console.log(flightNumberSearchEl)
+
+// })
 
 
 
