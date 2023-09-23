2e010e18db3b43db668046b831bd618f083915a0
Shocker <zacsmcdowell@gmail.com>
2023-09-23 17:23:39 -0400

    modified flight api to be in sync with user input/local.storagr

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,15 @@
+// api's to be used
+//  https://openskynetwork.github.io/opensky-api/rest.html 
+//  https://open-meteo.com/
+
+//-------------------------------------------------------------------------------------------
+
 // Variable Declaration area*********
 
 // these Var's are connected to the user input form on the search modal
-var flightNumberSearchEl = document.querySelector('#flight-search-input')
-var departureCitySearchEl = document.querySelector('#departure-search-input')
-var arrivalCitySearchEl = document.querySelector('#arrival-search-input')
+var flightInputVal = document.querySelector('#flight-search-input').value;
+var departureCityVal = document.querySelector('#departure-search-input').value;
+var arrivalCityVal = document.querySelector('#arrival-search-input').value;
 
 // previous searches table found in search modal 
 var flightFormEl = document.getElementById('flight-form')
@@ -11,7 +17,9 @@ var searchTableBodyEl = document.getElementById('table-body')
 var clearSearchHistoryBtnEl = document.getElementById('clear-search-history')
 var cancelModalBtnEl = document.getElementById('modal-cancel')
 var modalSearchBtnEl = document.querySelector('#modal-search')
-console.log(flightFormEl)
+var previousSearchNumber = document.getElementById('')
+var previousDepartureCity = document.getElementById('')
+var previousArrivalCity = document.getElementById('')
 
 
 // these Var's are the display below the header and above weather 
@@ -36,58 +44,74 @@ var arrivalWeatherDay3el = document.getElementById('arrival-day-3')
 var arrivalWeatherDay4el = document.getElementById('arrival-day-4')
 var arrivalWeatherDay5el = document.getElementById('arrival-day-5')
 
+//-------------------------------------------------------------------------------------------
 
 // Declaring the "search object" so it can be prepared for later use in the API's
 //and for storage/ recall of previous searches
 
-// function flightSearch(){
-
-//     var previousFlightsObj = {
-//         number: flightNumberSearchEl.value.trim(),
-//         departure: departureCitySearchEl.value.trim(),
-//         arrival : arrivalCitySearchEl.value.trim(),
-//     }
-
-// var previousFlightsObj = ('')
-
-// previousFlightsObj.push(number,departure,arrival)
-
-
-
-// var previousFlightArray = ('')
-// previousFlightArray.push(previousFlightsObj)
-// }
+var previousFlightsObj = []
 
 
 
 
-})
 
-// api's to be used
-//  https://openskynetwork.github.io/opensky-api/rest.html 
-//  https://open-meteo.com/
+function searchFormSubmit() {
+ 
+  
+    var flightInputVal = document.querySelector('#flight-search-input').value;
+    var departureCityVal = document.querySelector('#departure-search-input').value;
+    var arrivalCityVal = document.querySelector('#arrival-search-input').value;
+  
+    console.log("tacos")
+    console.log(flightInputVal)
+    console.log(departureCityVal)
+    console.log(arrivalCityVal)
 
+   
 
+        previousFlightsObj = {
+            number: flightInputVal,
+            departure: departureCityVal,
+            arrival : arrivalCityVal,
+        }
+    
+    localStorage.setItem("previousFlightsObj", JSON.stringify(previousFlightsObj));
+    
+}
+flightFormEl.addEventListener('submit', searchFormSubmit);
 
-// Flight API Section********
+function storeSearch () {
+
+    previousFlightsObj = {
+        number: flightInputVal,
+        departure: departureCityVal,
+        arrival : arrivalCityVal,
+    }
+
+localStorage.setItem("previousFlightsObj", JSON.stringify(previousFlightsObj));
+
+}
+
+//-------------------------------------------------------------------------------------------
+
+// // Flight API Section********
+
+const airportCode = flightInputVal
 
 function calculateTimeWindow() {
-    const now = new Date();
-    const beginTime = Math.floor((now.getTime() / 1000) - 3600); // One-hour window
-    const endTime = Math.floor(now.getTime() / 1000);
+    const now = new Date()
+    const beginTime = Math.floor((now.getTime() / 1000) - 3600) // One-hour window
+    const endTime = Math.floor(now.getTime() / 1000)
 
-    return { beginTime, endTime };
+    return { beginTime, endTime }
 }
-    
 
-
-function fetchDepartures(airportCode) {
-    const { beginTime, endTime } = calculateTimeWindow();
-    const departures = 'https://opensky-network.org/api/flights/departure?airport=${airportCode}&begin=${beginTime}&end=${endTime}'
+function fetchDepartures() {
+    // console.log("airportCode in fetch method" + airportCode)
+    const { beginTime, endTime } = calculateTimeWindow()
+    const departures = `https://opensky-network.org/api/flights/departure?airport=${airportCode}&begin=${beginTime}&end=${endTime}`
     
-    fetch(departures, {
-        mode: 'no-cors'
-    })
+    fetch(departures)
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`)
@@ -95,29 +119,23 @@ function fetchDepartures(airportCode) {
         return response.json()
     })
     .then(data => {
-        // Display the Airport Code in departureCityPrintEl
-        departureCityPrintEl.textContent = airportCode
+        // Display the Airport Code in flightInputVal
+        flightInputVal.textContent = airportCode
     })
     .catch(error => {
         console.error('Fetch Error:', error)
     })
+
+    searchFormSubmit(flightInputVal)
 }
 
-flightFormEl.addEventListener('submit', (e) => {
-    e.preventDefault()
 
-    const airportCode = departureCitySearchEl.value.trim()
-    
-    if (airportCode) {
-        // Fetch departures based on the entered airport code
-        fetchDepartures(airportCode)
-    }
-})
-fetchDepartures()
 
-// // Weather API Section **************
+// fetchDepartures()
 
+//-------------------------------------------------------------------------------------------
 
+// // // Weather API Section **************
 
 
 
@@ -209,30 +227,19 @@ fetchDepartures()
 
 
 
-var requestUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
-function testFetch() {
 
 
-fetch(requestUrl)
-.then(function (response) {
-return response.json();
-})
-.then(function (data) {
-console.log(data);
-});             
-}
-
-testFetch();
-
-
-
-
-
-
-
-
-
-
-
-
-
+// var requestUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+// function testFetch() {
+
+
+// fetch(requestUrl)
+// .then(function (response) {
+// return response.json();
+// })
+// .then(function (data) {
+// console.log(data);
+// });             
+// }
+
+// testFetch();
